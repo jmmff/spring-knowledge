59b53fd0f0a61136fed6b92b93630ff566bf3815
jmmff <jpfmarques96@gmail.com>
2020-04-08 22:27:39 +0100

    added spring-timer project skeleton

---
+++ spring-timer/src/test/java/com/ab3c3/springtimerexample/SpringTimerExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ab3c3.springtimerexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTimerExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
